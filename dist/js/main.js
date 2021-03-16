1135294ec63fe6fad31bf8f926a9e8243afd1bb5
Hristijan Petrovski <kiko-bt@hotmail.com>
2021-03-16 19:59:06 +0100

    method POST added

--- dist/js/main.js
+++ dist/js/main.js
@@ -220,6 +220,23 @@ document.getElementById('year').innerHTML = new Date().getFullYear();
 
 
 
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  let myForm = document.getElementById('contactMe');
+  let formData = new FormData(myForm)
+  fetch('/', {
+    method: 'POST',
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  }).then(() => console.log('Form successfully submitted')).catch((error) =>
+    alert(error))
+}
+
+
+
+
 
 
 
