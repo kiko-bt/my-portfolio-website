a65546d64bff1d3f3aaa10273401a9f6599bbdd9
Hristijan Petrovski <kiko-bt@hotmail.com>
2021-03-17 02:43:12 +0100

    fix some error

--- dist/js/main.js
+++ dist/js/main.js
@@ -227,14 +227,14 @@ const emailValidation = document.getElementById('emailValidation');
     // Loop over them and prevent submission
     var validation = Array.prototype.filter.call(forms, function(form) {
       form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
+        if(form.checkValidity() === false) {
           event.preventDefault();
           event.stopPropagation();
         }
+        form.classList.add('was-validated');
         if(!isValidEmail(email.value)) {
           emailValidation.innerHTML = 'Email is not valid';
         }
-        form.classList.add('was-validated');
       }, false);
     });
   }, false);
